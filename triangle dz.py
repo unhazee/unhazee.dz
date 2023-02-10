bc57165cf7e804f414920c9d6f954e894d9270d7
unhazee <denkononden@gmail.com>
2023-02-10 11:06:38 +0200

    дз сум з лістом

--- triangle dz.py
+++ triangle dz.py
@@ -1,13 +1,13 @@
 def triangle():
-    a = float(input("Введіть довжину першої сторони: "))  # Запрошую данні першої сторони
-    b = float(input("Введіть довжину другої сторони: "))  # Запрошую данні другої сторони
-    c = float(input("Введіть довжину третьої сторони: "))  # Запрошую данні третьої сторони
-    if a + b > c and b + c > a and a + c > b:  # Якщо данні відповідають пропорціям , обичслюєм :
-        p = (a + b + c) / 2  # пів периметр
-        s = pow(p * (p - a) * (p - b) * (p - c), 1 / 2)  # і площу
-        answer = (f"Площа трикутника зі сторонами {a}, {b}, {c} складає: {s}")
+    first_side = float(input("Введіть довжину першої сторони: "))  # Запрошую данні першої сторони
+    second_side = float(input("Введіть довжину другої сторони: "))  # Запрошую данні другої сторони
+    third_side = float(input("Введіть довжину третьої сторони: "))  # Запрошую данні третьої сторони
+    if first_side + second_side > third_side and second_side + third_side > first_side and first_side + third_side > second_side:  # Якщо данні відповідають пропорціям , обичслюєм :
+        half_perimeter = (first_side + second_side + third_side) / 2  # пів периметр
+        s = pow(half_perimeter * (half_perimeter - first_side) * (half_perimeter - second_side) * (half_perimeter - third_side), 1 / 2)  # і площу
+        answer = (f"Площа трикутника зі сторонами {first_side}, {second_side}, {third_side} складає: {s}")
     else:  # В інакшому випадку вибиваєм текст що данні не коректін
-        answer = (f'Трикутник зі сторонами {a}, {b}, {c} існувати не може. И площі у нього також немає :)')
+        answer = (f'Трикутник зі сторонами {first_side}, {second_side}, {third_side} існувати не може. И площі у нього також немає :)')
     return answer
 answer = triangle()
 print(answer)
